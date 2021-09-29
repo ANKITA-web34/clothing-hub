151eb06f298c730a19967c0996a7ac753bab9e66
Ankita <anki333soul@gmail.com>
2021-09-29 01:57:44 -0700

    added shop page and shop data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/homepage/homepage.component';
+import ShopPage from './Pages/Shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route  path='/hats' component={HatsPage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
