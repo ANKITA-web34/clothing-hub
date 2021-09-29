77f00aec99f6c14dc502f5a5f8b21c33adaea323
Ankita <anki333soul@gmail.com>
2021-09-28 23:05:09 -0700

    Route withRouter()

--- src/Pages/homepage/homepage.component.jsx
+++ src/Pages/homepage/homepage.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import './homepage.styles.scss';
 import Directory from '../../Components/directory/directory.componet';
 
+
 const HomePage = () => (
     <div className="homepage">
      <Directory />
