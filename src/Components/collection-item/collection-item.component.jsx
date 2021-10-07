4e0b447ce43d962cd9c3fba35d0a06452b59188a
Ankita <anki333soul@gmail.com>
2021-10-07 15:25:54 -0700

    cartItem reducer

--- src/Components/collection-item/collection-item.component.jsx
+++ src/Components/collection-item/collection-item.component.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../Custom-button/custom-button.component";
+import { addItem } from "../../Redux/Cart/cart.actions";
+
 import './collection-item.styles.scss';
 
 const CollectionItem =({ id, name, price, imageUrl}) => (
@@ -9,8 +14,13 @@ const CollectionItem =({ id, name, price, imageUrl}) => (
         <div className="collection-footer">
             <span className='name'>{ name }</span>
             <span className='price'>{ price }</span>
-        </div>         
+        </div>    
+        <CustomButton inverted>Add To Cart</CustomButton>     
     </div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+});
+
+export default connect(mapDispatchToProps)(CollectionItem);
