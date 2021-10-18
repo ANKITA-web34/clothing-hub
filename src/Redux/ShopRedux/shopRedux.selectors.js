e5d1e7c4b2ad1db44563b256df85aabb4446ac84
Ankita <anki333soul@gmail.com>
2021-10-17 22:55:02 -0700

    collection routing and selector and data normalization + collection page and data flow in our app

--- src/Redux/ShopRedux/shopRedux.selectors.js
+++ src/Redux/ShopRedux/shopRedux.selectors.js
@@ -1,22 +1,20 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-    hats: 1,
-    sneakers: 2,
-    jackects: 3,
-    womens: 4,
-    mens: 5
-}
-
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
-    [selectShop],
-    shop => shop.collections
+  [selectShop],
+  (shop) => shop.collections
 );
 
-export const selectionCollection = collectionUrlParam => createSelector(
-    [selectionCollection],
-    collections => collections.find(collection => 
-        collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections)
+  .map(key => collections[key])
 );
+
+export const selectCollection = collectionUrlParam => 
+  createSelector(
+      [selectCollections],
+      collections => collections[collectionUrlParam]
+  );
