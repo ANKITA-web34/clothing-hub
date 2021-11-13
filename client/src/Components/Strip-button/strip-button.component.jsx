e244d42b9ff9ea7fe9651d4d95dc383211fd0bbf
Ankita <anki333soul@gmail.com>
2021-11-12 18:10:35 -0700

    remove gitignore files

--- client/src/Components/Strip-button/strip-button.component.jsx
+++ client/src/Components/Strip-button/strip-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,21 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51JlykhSB4gPcpuXt9SaSIVqAcwRT5taKVjyGkY2q4XkbyBzyQ32hzy8XWCwWTPqPgDRkU1qbD0AzFPi7ApPVEEMi00DD6BN9p7";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("payment successful");
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('payment successful')
+    }).catch(error => {
+      console.log('payment error: ', JSON.parse(error))
+      alert(
+        'There is an issue with your payment. Please sure you are the provided credit card'
+        )
+    })
   };
 
   return (
